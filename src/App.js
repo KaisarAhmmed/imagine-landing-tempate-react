9285210bca1cc8808cc9f91444f1d64ad35509a2
Kaisar Ahmmed <kaisar123k@gmail.com>
2022-01-22 21:04:58 +0600

    add tailwind

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-import "./App.css";
-
 function App() {
-    return <h2>start here</h2>;
+    return (
+        <div>
+            <h1 className="text-3xl font-bold underline">Hello world!</h1>
+            <div className="bg-gray-400">lkds</div>
+        </div>
+    );
 }
 
 export default App;
